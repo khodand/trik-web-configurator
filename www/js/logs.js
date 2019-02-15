e19319faaaaba897701466286a8b9a69a337b0a2
Andrei Khodko <khodand@outlook.com>
2019-02-15 19:33:47 +0300

    fix for unred element

--- www/js/logs.js
+++ www/js/logs.js
@@ -1,13 +1,16 @@
 setInterval(function() {
 	var elementsByClass = document.getElementsByClassName("n");
 	for (var i = 2; i < elementsByClass.length; i++) {
-		elements = x[i].getElementsByTagName("a");
+		var elements = x[i].getElementsByTagName("a");
 		var xhr = new XMLHttpRequest();
 		xhr.open("GET", "/cgi-bin/logs.sh", false);
 		xhr.setRequestHeader('Content-Type', 'text-plain');
 		xhr.send(elements[0].innerHtml);
 		if (xhr.responseText === "True") {
 			elements[0].style.background = "red";
+		}
+		else {
+			elements[0].style.background = "green";
 		}
 	}
 }
